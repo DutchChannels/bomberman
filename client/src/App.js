1d6096b59e6ee0bb0f922b519c5a8979a9011804
Emile Broeders <emile.broeders@dutchchannels.nl>
2022-03-10 19:39:28 +0100

    feat(client): Added a name input box

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ name, setName ] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setName(e.target.name.value);
+  };
+
+  const inner = (() => {
+    if (name.length < 1)
+      return (
+        <form onSubmit={handleSubmit}>
+          <p><input className="input is-primary is-large my-2" type="text" name="name" placeholder="What's your name?" /></p>
+          <p><input className="button is-primary is-large mt-2" type="submit" value="Join!" /></p>
+        </form>
+      );
+    else
+      return 'Waiting to start the game';
+  })();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="is-size-1 mb-2">Bomberman</h1>
+        {inner}
       </header>
     </div>
   );
