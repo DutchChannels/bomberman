7086582798834a35b6c6094f4b342989cb00626e
JesseSchouten <jesse.schouten@outlook.com>
2022-03-10 20:25:29 +0100

    v1

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 import { io } from "socket.io-client";
 
-const socket = io("wss://localhost:3001");
+const socket = io("http://localhost:3001");
 
 socket.on("connect", () => {
   console.log('Connected!');
